cc6a1e343499c41935c387f13d47cc939fa450ee
Yuri Barbosa <127057738+YuriGit909@users.noreply.github.com>
2024-09-12 20:36:54 -0300

    Maked goals area and menu application

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 // IMPORTAÇÃO DE UM MÓDULO (OU DEPENDÊNCIA) EXTERNO
-const { select } = require("@inquirer/prompts");
+const { select, input } = require("@inquirer/prompts");
 
-const start = () => {
+let meta = {
+    value: "Beber 3L de água todos os dias",
+    checked: false,
+}
+
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input({ message: "Digite a meta: " })
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia.")
+        return;
+    }
+
+    metas.push({ 
+        value: meta, 
+        checked: false 
+    })
+}
+
+const start = async () => {
     while(true) {
-        let opcao = 'Sair'
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar metas",
+                    value: "cadastrar",
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar",
+                },
+                {
+                    name: "Sair",
+                    value: "sair",
+                },
+            ],
+        })
+
         switch(start){
-        case 'Cadastrar':
-            console.log("Vamos cadastrar!")
+        case 'cadastrar':
+            await cadastrarMeta()
+            console.log(metas)
             break;
         case "listar":
             console.log("Vamos listar")
             break;
-        case 'Sair':
+        case 'sair':
             return
         }
     }
